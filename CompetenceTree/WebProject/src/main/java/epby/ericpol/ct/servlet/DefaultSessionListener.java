e6e47675a7f289d3c8054e31f76ed7a5f56babf7
xpst <statpavel@gmail.com>
2014-03-31 23:32:48 +0300

    Format code

--- CompetenceTree/WebProject/src/main/java/epby/ericpol/ct/servlet/DefaultSessionListener.java
+++ CompetenceTree/WebProject/src/main/java/epby/ericpol/ct/servlet/DefaultSessionListener.java
@@ -6,27 +6,21 @@ import javax.ejb.EJB;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-
 public final class DefaultSessionListener implements HttpSessionListener
 {
     public static final String SESSION_ATTRIBUTE = "UserSessionService";
     private static final Logger LOGGER = Logger.getLogger(DefaultSessionListener.class.getName());
-
 
     @Override
     public void sessionCreated(HttpSessionEvent arg0)
     {
         LOGGER.finest("sessionCreated");
 
-        /*if (itsService != null)
-        {
-            arg0.getSession().setAttribute(SESSION_ATTRIBUTE, itsService);
-            LOGGER.warning("Session bean stored in HTTP session.");
-        }
-        else
-        {
-            LOGGER.warning("There is no reference to session bean");
-        }*/
+        /*
+         * if (itsService != null) { arg0.getSession().setAttribute(SESSION_ATTRIBUTE, itsService);
+         * LOGGER.warning("Session bean stored in HTTP session."); } else {
+         * LOGGER.warning("There is no reference to session bean"); }
+         */
 
     }
 
--- CompetenceTree/WebProject/src/main/java/epby/ericpol/ct/servlet/TestServlet.java
+++ CompetenceTree/WebProject/src/main/java/epby/ericpol/ct/servlet/TestServlet.java
@@ -22,7 +22,6 @@ public final class TestServlet extends HttpServlet
     private static final long serialVersionUID = 1L;
     private static final Logger LOGGER = Logger.getLogger(TestServlet.class.getName());
 
-
     /**
      * @see HttpServlet#HttpServlet()
      */
